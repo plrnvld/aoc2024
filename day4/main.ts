31019f7753e0af837283299fbbd031318cd6f6dd
Paul Rijneveld <paul.rijneveld@mendix.com>
2024-12-05 00:13:04 +0100

    Solve part 2

--- day4/main.ts
+++ day4/main.ts
@@ -1,51 +1,67 @@
-function getChar(row: number, column: number, lines: string[]): string | undefined {
-  if (row < 0 || row >= lines.length)
+function getChar(
+  row: number,
+  column: number,
+  lines: string[],
+): string | undefined {
+  if (row < 0 || row >= lines.length) {
     return undefined;
-  if (column < 0 || column >= lines[row].length)
+  }
+  if (column < 0 || column >= lines[row].length) {
     return undefined;
+  }
 
   return lines[row].charAt(column);
 }
 
-function matchEast(rowStart: number, columnStart: number, lines: string[]) {
-  return matchXMAS(rowStart, columnStart, 1, 0, lines);
+function matchSouthEastMAS(
+  centerRow: number,
+  centerCol: number,
+  lines: string[],
+) {
+  return getChar(centerRow - 1, centerCol - 1, lines) === "M" &&
+    getChar(centerRow, centerCol, lines) === "A" &&
+    getChar(centerRow + 1, centerCol + 1, lines) === "S";
 }
 
-function matchWest(rowStart: number, columnStart: number, lines: string[]) {
-  return matchXMAS(rowStart, columnStart, -1, 0, lines);
+function matchNorthWestMAS(
+  centerRow: number,
+  centerCol: number,
+  lines: string[],
+) {
+  return getChar(centerRow - 1, centerCol - 1, lines) === "S" &&
+    getChar(centerRow, centerCol, lines) === "A" &&
+    getChar(centerRow + 1, centerCol + 1, lines) === "M";
 }
 
-function matchNorth(rowStart: number, columnStart: number, lines: string[]) {
-  return matchXMAS(rowStart, columnStart, 0, -1, lines);
+function matchNorthEastMAS(
+  centerRow: number,
+  centerCol: number,
+  lines: string[],
+) {
+  return getChar(centerRow + 1, centerCol - 1, lines) === "M" &&
+    getChar(centerRow, centerCol, lines) === "A" &&
+    getChar(centerRow - 1, centerCol + 1, lines) === "S";
 }
 
-function matchSouth(rowStart: number, columnStart: number, lines: string[]) {
-  return matchXMAS(rowStart, columnStart, 0, 1, lines);
+function matchSouthWestMAS(
+  centerRow: number,
+  centerCol: number,
+  lines: string[],
+) {
+  return getChar(centerRow + 1, centerCol - 1, lines) === "S" &&
+    getChar(centerRow, centerCol, lines) === "A" &&
+    getChar(centerRow - 1, centerCol + 1, lines) === "M";
 }
 
-function matchNorthEast(rowStart: number, columnStart: number, lines: string[]) {
-  return matchXMAS(rowStart, columnStart, 1, -1, lines);
+function matchSlash(centerRow: number, centerCol: number, lines: string[]) {
+  return matchNorthEastMAS(centerRow, centerCol, lines) ||
+    matchSouthWestMAS(centerRow, centerCol, lines);
 }
 
-function matchSouthEast(rowStart: number, columnStart: number, lines: string[]) {
-  return matchXMAS(rowStart, columnStart, 1, 1, lines);
+function matchBackslash(centerRow: number, centerCol: number, lines: string[]) {
+  return matchSouthEastMAS(centerRow, centerCol, lines) ||
+    matchNorthWestMAS(centerRow, centerCol, lines);
 }
-
-function matchNorthWest(rowStart: number, columnStart: number, lines: string[]) {
-  return matchXMAS(rowStart, columnStart, -1, -1, lines);
-}
-
-function matchSouthWest(rowStart: number, columnStart: number, lines: string[]) {
-  return matchXMAS(rowStart, columnStart, -1, 1, lines);
-}
-
-function matchXMAS(rowStart: number, columnStart: number, deltaRow: number, deltaCol: number, lines: string[]): boolean {
-  return getChar(rowStart, columnStart, lines) === 'X' 
-    && getChar(rowStart + deltaRow, columnStart + deltaCol, lines) === 'M'
-    && getChar(rowStart + 2 * deltaRow, columnStart + 2 * deltaCol, lines) === 'A'
-    && getChar(rowStart + 3 * deltaRow, columnStart + 3 * deltaCol, lines) === 'S';
-}
-
 
 if (import.meta.main) {
   const text = await Deno.readTextFile("input");
@@ -55,25 +71,9 @@ if (import.meta.main) {
 
   for (let y = 0; y < lines.length; y++) {
     for (let x = 0; x < lines[y].length; x++) {
-
-      console.log(`Checking row=${y}, col=${x}`);
-
-      if (matchEast(y, x, lines))
+      if (matchSlash(y, x, lines) && matchBackslash(y, x, lines)) {
         count += 1;
-      if (matchSouthEast(y, x, lines))
-        count += 1;
-      if (matchSouth(y, x, lines))
-        count += 1;
-      if (matchSouthWest(y, x, lines))
-        count += 1;
-      if (matchWest(y, x, lines))
-        count += 1;
-      if (matchNorthWest(y, x, lines))
-        count += 1;
-      if (matchNorth(y, x, lines))
-        count += 1;
-      if (matchNorthEast(y, x, lines))
-        count += 1;
+      }
     }
   }
 
