b8912b6328c596836f81f2abe05f6feb075e1b2f
Paul Rijneveld <paul.rijneveld@mendix.com>
2024-12-04 22:06:06 +0100

    Part 1

--- day3/main.ts
+++ day3/main.ts
@@ -1,8 +1,28 @@
-export function add(a: number, b: number): number {
-  return a + b;
+function readInstructions(line: string): number {
+  const pattern = /mul\([0-9]+\,[0-9]+\)/g;
+  const res = line.match(pattern);
+
+  let sum = 0;
+
+  if (res) {
+    for (const group of res) {
+      // console.log("group > " + group);
+      const nums = group.match(/[0-9]+/g);
+
+      if (nums) 
+        sum += parseInt(nums[0]) * parseInt(nums[1]);
+    }
+  }
+ 
+  return sum;
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+// mul(123,4)
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  const text = await Deno.readTextFile("input");
+  const lines: string[] = text.split("\n");
+
+  const total = lines.map(line => readInstructions(line)).reduce((a, b) => a + b, 0);
+
+  console.log(total);
 }
