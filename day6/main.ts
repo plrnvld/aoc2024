8ffe998d8a3bf6ff630807de60d1868c500ecd99
Paul Rijneveld <paul.rijneveld@mendix.com>
2024-12-06 13:01:54 +0100

    Read input

--- day6/main.ts
+++ day6/main.ts
@@ -1,6 +1,26 @@
+
+
 if (import.meta.main) {
   const text = await Deno.readTextFile("example");
   const lines: string[] = text.split("\n");
 
-  console.log(text);
+  const obstacles = new Set<[number, number]>();
+
+  let position: [number, number] | undefined = undefined;
+
+  for (let row = 0; row < lines.length; row++) {
+    for (let col = 0; col < lines[row].length; col++) {
+      const elem = lines[row].charAt(col);
+
+      if (elem === "#")
+        obstacles.add([col, row]);
+      else if (elem === "^")
+        position = [col, row];
+    }
+  }
+
+  if (!position)
+    throw new Error("No position found");
+
+  console.log("Obstacle count = " + obstacles.size);
 }
