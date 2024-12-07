09e9db385367920b6f8847281b29d05fc784f0bc
Paul Rijneveld <paul.rijneveld@mendix.com>
2024-12-07 13:35:07 +0100

    Solve part 2

--- day7/main.ts
+++ day7/main.ts
@@ -1,7 +1,7 @@
 class Equation {
   target: number;
   numbers: number[];
-  
+
   constructor(target: number, numbers: number[]) {
     this.target = target;
     this.numbers = numbers;
@@ -25,7 +25,7 @@ class SubResult {
 function parseEquation(line: string): Equation {
   const parts = line.split(": ");
   const target = parseInt(parts[0]);
-  const numbers = parts[1].split(" ").map(t => parseInt(t));
+  const numbers = parts[1].split(" ").map((t) => parseInt(t));
 
   return new Equation(target, numbers);
 }
@@ -40,13 +40,17 @@ function isSolvable(equation: Equation) {
 
     if (curr.isFinished(equation.numbers.length)) {
       if (curr.currentValue === equation.target) {
-        console.log("Target " + equation.target + " can be solved.")
+        console.log("Target " + equation.target + " can be solved.");
         return true;
       }
-    } else if (curr.currentValue < equation.target) {
+    } else if (curr.currentValue <= equation.target) {
       const nextNumber = equation.numbers[curr.nextIndex];
-      stack.push(new SubResult(curr.currentValue + nextNumber, curr.nextIndex + 1));
-      stack.push(new SubResult(curr.currentValue * nextNumber, curr.nextIndex + 1));
+      stack.push(
+        new SubResult(curr.currentValue + nextNumber, curr.nextIndex + 1),
+      );
+      stack.push(
+        new SubResult(curr.currentValue * nextNumber, curr.nextIndex + 1),
+      );
     }
   }
 
@@ -56,17 +60,15 @@ function isSolvable(equation: Equation) {
 if (import.meta.main) {
   const text = await Deno.readTextFile("input");
   const lines: string[] = text.split("\n");
-  const equations = lines.map(l => parseEquation(l));
+  const equations = lines.map((l) => parseEquation(l));
 
   let sum = 0;
 
   for (const equation of equations) {
-    if (isSolvable(equation))
+    if (isSolvable(equation)) {
       sum += equation.target;
+    }
   }
 
   console.log(sum);
 }
-
-
-// 5030884341842 too low
