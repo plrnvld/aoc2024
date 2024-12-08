a71a0c12bed6e2734b1afc2d450686e4a992a818
Paul Rijneveld <paul.rijneveld@mendix.com>
2024-12-08 13:27:36 +0100

    Solve day 8 part 1

--- day8/main.ts
+++ day8/main.ts
@@ -1,14 +1,77 @@
 class Pos {
   row: number;
-  pos: number;
+  col: number;
 
-  constructor(row: number, pos: number) {
+  constructor(row: number, col: number) {
     this.row = row;
-    this.pos = pos;
+    this.col = col;
   }
 }
 
-async function readAntennas(fileName: string): Promise<Map<string, Pos[]>> {
+class AntennaMap {
+  width: number;
+  height: number;
+  antennas: Map<string, Pos[]>;
+
+  constructor(antennas: Map<string, Pos[]>, width: number, height: number) {
+    this.antennas = antennas;
+    this.width = width;
+    this.height = height;
+  }
+
+  inBounds(pos: Pos) {
+    return pos.row >= 0 && pos.row < this.height && pos.col >= 0 &&
+      pos.col < this.width;
+  }
+}
+
+function combinations(positions: Pos[]): [Pos, Pos][] {
+  const combs: [Pos, Pos][] = [];
+
+  for (let i = 0; i < positions.length; i++) {
+    for (let j = i + 1; j < positions.length; j++) {
+      combs.push([positions[i], positions[j]]);
+    }
+  }
+
+  return combs;
+}
+
+function antinodes(position1: Pos, position2: Pos): Pos[] {
+  const diffRow = position2.row - position1.row;
+  const diffCol = position2.col - position1.col;
+
+  const antinode1 = new Pos(position1.row - diffRow, position1.col - diffCol);
+  const antinode2 = new Pos(position2.row + diffRow, position2.col + diffCol);
+
+  return [antinode1, antinode2];
+}
+
+function rowColToKey(row: number, col: number): number {
+  return row * 1000 + col;
+}
+
+function countAntinodes(antennaMap: AntennaMap): number {
+  const antinodeSet: Set<number> = new Set();
+
+  for (const antennaName of antennaMap.antennas.keys()) {
+    const combs = combinations(antennaMap.antennas.get(antennaName)!);
+
+    for (const comb of combs) {
+      const antisInBounds = antinodes(comb[0], comb[1]).filter((a) =>
+        antennaMap.inBounds(a)
+      );
+
+      for (const antiInBounds of antisInBounds) {
+        antinodeSet.add(rowColToKey(antiInBounds.row, antiInBounds.col));
+      }
+    }
+  }
+
+  return antinodeSet.size;
+}
+
+async function readAntennas(fileName: string): Promise<AntennaMap> {
   const text = await Deno.readTextFile(fileName);
   const lines: string[] = text.split("\n");
 
@@ -18,13 +81,12 @@ async function readAntennas(fileName: string): Promise<Map<string, Pos[]>> {
     for (let col = 0; col < lines[row].length; col++) {
       const c = lines[row].at(col)!;
 
-      const newPos = new Pos(row, col)
+      const newPos = new Pos(row, col);
 
       if (c !== ".") {
-        const currentEntry = antennas.get(c);
-        if (currentEntry) {
-          const newPositions = currentEntry.concat(newPos);
-          antennas.set(c, newPositions);
+        const currentPositions = antennas.get(c);
+        if (currentPositions) {
+          antennas.set(c, currentPositions.concat(newPos));
         } else {
           antennas.set(c, [newPos]);
         }
@@ -32,14 +94,13 @@ async function readAntennas(fileName: string): Promise<Map<string, Pos[]>> {
     }
   }
 
-  return antennas;
+  return new AntennaMap(antennas, lines[0].length, lines.length);
 }
 
 if (import.meta.main) {
-  const antennas = await readAntennas("example");
-  
+  const antennaMap = await readAntennas("input");
 
-  for (const key of antennas.keys()) {
-    console.log(`[${key}]: ${antennas.get(key)?.map(p => JSON.stringify(p))}`);
-  }
+  const count = countAntinodes(antennaMap);
+
+  console.log(count);
 }
