ac5bb4550786c3faf09e4658e15ecc2055516d8c
Paul Rijneveld <paul.rijneveld@mendix.com>
2024-12-09 20:52:46 +0100

    Calculate block size

--- day9/main.ts
+++ day9/main.ts
@@ -15,12 +15,21 @@ function freeSpaceAfter(blockId: number, diskMap: string) {
   return parseInt(diskMap[freeSpaceIndex]);
 }
 
+function blockSize(blockId: number, diskMap: string) {
+  const blockIndex = blockId * 2;
+
+  if (blockIndex >= diskMap.length)
+    throw new Error(`blockIndex ${blockIndex} out of bounds`);
+  
+  return parseInt(diskMap[blockIndex]);
+}
+
 if (import.meta.main) {
   const diskMap = await Deno.readTextFile("example");
   const numBlocks = findNumBlocks(diskMap);
   
   for (let i = 0; i < numBlocks; i++) {
-    console.log(`Block ${i} trailed by ${freeSpaceAfter(i, diskMap)} spaces.`)
+    console.log(`Block ${i.toString().repeat(blockSize(i, diskMap))} trailed by ${freeSpaceAfter(i, diskMap)} spaces.`)
   }
 
   console.log(findNumBlocks(diskMap));
