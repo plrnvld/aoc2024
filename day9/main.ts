d6fc213f18a294c623bf5d43b9879faf1272e278
Paul Rijneveld <paul.rijneveld@mendix.com>
2024-12-09 23:45:53 +0100

    Solve day 9 part 1

--- day9/main.ts
+++ day9/main.ts
@@ -1,4 +1,3 @@
-
 function findNumBlocks(diskMap: string) {
   return (diskMap.length + 1) / 2;
 }
@@ -6,31 +5,40 @@ function findNumBlocks(diskMap: string) {
 function freeSpaceAfter(blockId: number, diskMap: string) {
   const freeSpaceIndex = 1 + blockId * 2;
 
-  if (freeSpaceIndex == diskMap.length)
+  if (freeSpaceIndex == diskMap.length) {
     return 0; // After last block there is no space
+  }
 
-  if (freeSpaceIndex > diskMap.length)
+  if (freeSpaceIndex > diskMap.length) {
     throw new Error(`freeSpaceIndex ${freeSpaceIndex} out of bounds`);
-  
+  }
+
   return parseInt(diskMap[freeSpaceIndex]);
 }
 
 function blockSize(blockId: number, diskMap: string) {
   const blockIndex = blockId * 2;
 
-  if (blockIndex >= diskMap.length)
+  if (blockIndex >= diskMap.length) {
     throw new Error(`blockIndex ${blockIndex} out of bounds`);
-  
+  }
+
   return parseInt(diskMap[blockIndex]);
 }
 
 if (import.meta.main) {
-  const diskMap = await Deno.readTextFile("example");
+  const diskMap = await Deno.readTextFile("input");
   const numBlocks = findNumBlocks(diskMap);
-  
+
   for (let i = 0; i < numBlocks; i++) {
-    console.log(`Block ${i.toString().repeat(blockSize(i, diskMap))} trailed by ${freeSpaceAfter(i, diskMap)} spaces.`)
+    console.log(
+      `Block ${i.toString().repeat(blockSize(i, diskMap))} trailed by ${
+        freeSpaceAfter(i, diskMap)
+      } spaces.`,
+    );
   }
+
+  console.log();
 
   let righBlockId = numBlocks - 1;
   let leftBlockId = 0;
@@ -40,31 +48,55 @@ if (import.meta.main) {
   let checkSum = 0;
 
   while (leftBlockId <= righBlockId) { // Don't count the last right block items twice
-    for (let j = 0; j < blockSize(leftBlockId, diskMap); j++) {
+    for (let j = 0; j < blockSize(leftBlockId, diskMap) && (leftBlockId !== righBlockId || j < rightBlockRemaining); j++) {
       const addLeft = leftIndex * leftBlockId;
 
-      console.log(`Adding ${leftIndex} * ${leftBlockId} = ${addLeft}`)
+      console.log(`L: Adding ${leftIndex} * ${leftBlockId} = ${addLeft}`);
 
-      checkSum += addLeft
+      if (leftBlockId === righBlockId) {
+        rightBlockRemaining--;
+      }
+
+      checkSum += addLeft;
       leftIndex++;
     }
 
+    console.log("> Finished filling " + leftBlockId);
     leftBlockId++;
 
-    for (let k = 0; k < freeSpaceAfter(leftBlockId, diskMap) && leftIndex < rightIndex; k++) {
-      if (rightBlockRemaining == 0) {
-        righBlockId--;
-        rightBlockRemaining = blockSize(righBlockId, diskMap);
+
+    if (leftBlockId < righBlockId) {
+      for (
+        let k = 0;
+        k < freeSpaceAfter(leftBlockId-1, diskMap);
+        k++
+      ) {
+        if (rightBlockRemaining == 0) {
+          righBlockId--;
+          rightBlockRemaining = blockSize(righBlockId, diskMap);
+        }
+
+        const addRight = leftIndex * righBlockId;
+
+        console.log(`R: Adding ${leftIndex} * ${righBlockId} = ${addRight}`);
+
+        checkSum += addRight;
+        rightBlockRemaining--;
+        leftIndex++;
+        rightIndex--;
+      }
+    } else { // final block
+      for (let m = 0; m < rightBlockRemaining; m++) {
+        const addFinal = leftIndex * righBlockId;
+
+        console.log(`FR: Adding ${leftIndex} * ${righBlockId} = ${addFinal}`);
+
+        checkSum += addFinal;
+
+        rightBlockRemaining--;
+        leftIndex++;
       }
 
-      const addRight = leftIndex * righBlockId;
-
-      console.log(`Adding ${leftIndex} * ${righBlockId} = ${addRight}`)
-
-      checkSum += addRight
-      rightBlockRemaining--;
-      leftIndex++;
-      rightIndex--;
     }
   }
 
