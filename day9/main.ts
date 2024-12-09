68cbc72a66cac17f5ae24c3656251a63f1b46e19
Paul Rijneveld <paul.rijneveld@mendix.com>
2024-12-09 20:49:43 +0100

    Calculate free space after block

--- day9/main.ts
+++ day9/main.ts
@@ -1,5 +1,27 @@
-if (import.meta.main) {
-  const text = await Deno.readTextFile("input");
 
-  console.log(text);
+function findNumBlocks(diskMap: string) {
+  return (diskMap.length + 1) / 2;
+}
+
+function freeSpaceAfter(blockId: number, diskMap: string) {
+  const freeSpaceIndex = 1 + blockId * 2;
+
+  if (freeSpaceIndex == diskMap.length)
+    return 0; // After last block there is no space
+
+  if (freeSpaceIndex > diskMap.length)
+    throw new Error(`freeSpaceIndex ${freeSpaceIndex} out of bounds`);
+  
+  return parseInt(diskMap[freeSpaceIndex]);
+}
+
+if (import.meta.main) {
+  const diskMap = await Deno.readTextFile("example");
+  const numBlocks = findNumBlocks(diskMap);
+  
+  for (let i = 0; i < numBlocks; i++) {
+    console.log(`Block ${i} trailed by ${freeSpaceAfter(i, diskMap)} spaces.`)
+  }
+
+  console.log(findNumBlocks(diskMap));
 }
