bbb58cb74ed304f284ad6acd846f103d44198e0f
Paul Rijneveld <paul.rijneveld@mendix.com>
2024-12-22 21:21:59 +0100

    Solve day 16 part 1

--- day16/main.ts
+++ day16/main.ts
@@ -55,7 +55,7 @@ class Graph {
         const c = this.getChar(x, y);
         const createdVertices: Vertex[] = [];
 
-        if (hasPathLeft || hasPathRight) {
+        if (hasPathLeft || hasPathRight || c === "S") {
           createdVertices.push(this.addNewVertex(x, y, "leftRight"));
 
           if (c === "S") {
@@ -66,6 +66,8 @@ class Graph {
         if (hasPathUp || hasPathDown) {
           createdVertices.push(this.addNewVertex(x, y, "upDown"));
         }
+
+        
 
         if (c === "E") {
           createdVertices.forEach((v) => this.targetVertexIds.push(v.id));
@@ -83,6 +85,7 @@ class Graph {
           }
 
           this.setDist(leftVertex.id, currVertex.id, 2);
+          this.addNeighbors(leftVertex.id, currVertex.id);
         }
 
         if (hasPathUp) { // Connect with previously added vertex up
@@ -99,10 +102,14 @@ class Graph {
           }
 
           this.setDist(upVertex.id, currVertex.id, 2);
+          this.addNeighbors(upVertex.id, currVertex.id);
         }
 
         if (createdVertices.length === 2) {
           this.setDist(createdVertices[0].id, createdVertices[1].id, 1000);
+          this.addNeighbors(createdVertices[0].id, createdVertices[1].id);
+        } else {
+          // console.log(`--> No two edges for ${x},${y}`);
         }
       }
     }
@@ -124,6 +131,21 @@ class Graph {
     return vertex;
   }
 
+  addNeighbors(vertexId1: number, vertexId2: number) {
+    this.addNeighborTo(vertexId1, vertexId2);
+    this.addNeighborTo(vertexId2, vertexId1);
+  }
+
+  addNeighborTo(neighborId: number, toVertexId: number) {
+    const neighbors = this.neighborsMap.get(toVertexId);
+    if (neighbors !== undefined) {
+      neighbors.push(neighborId);
+      this.neighborsMap.set(toVertexId, neighbors);
+    } else {
+      this.neighborsMap.set(toVertexId, [neighborId]);
+    }
+  }
+
   isPath(x: number, y: number) {
     const c = this.getChar(x, y);
     return c === "." || c === "S" || c === "E";
@@ -135,6 +157,15 @@ class Graph {
 
   getDist(fromVertexId: number, to: number): number | undefined {
     return this.costMap.get(this.getEdgeId(fromVertexId, to));
+  }
+
+  getVertex(id: number): Vertex {
+    const vertex = this.vertices[id];
+    if (vertex === undefined) {
+      throw new Error("Cannot find vertex " + id);
+    }
+
+    return vertex;
   }
 
   setDist(fromVertexId: number, to: number, dist: number) {
@@ -158,10 +189,64 @@ class Graph {
   }
 }
 
+function dijkstra(graph: Graph, startId: number, targetId: number): number {
+  const queue: number[] = [];
+  graph.vertices.forEach((v) => queue.push(v.id));
+  const startVertex = graph.getVertex(startId);
+  startVertex.dist = 0;
+
+  while (queue.length > 0) {
+    queue.sort((a, b) => {
+      const aDist = graph.getVertex(a).dist;
+      const bDist = graph.getVertex(b).dist;
+
+      // Reversed order, biggest first
+      if (aDist > bDist) {
+        return -1;
+      }
+      if (aDist < bDist) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    const uId = queue.pop()!; // Take last from reversed order is the smallest;
+    const neighbors = graph.neighbors(uId);
+    const u = graph.getVertex(uId);
+
+    if (u.id === targetId) {
+      return u.dist;
+    }
+
+    for (const neighborId of neighbors) {
+      const neighborDist = graph.getDist(uId, neighborId);
+      if (neighborDist === undefined) {
+        throw new Error("Neighbor dist cannot be empty");
+      }
+      const alt = u.dist + neighborDist;
+      const neighbor = graph.getVertex(neighborId);
+
+      if (alt < neighbor.dist) {
+        neighbor.dist = alt;
+        neighbor.prev = u;
+      }
+    }
+  }
+
+  throw new Error("Dijkstra didn't terminate");
+}
+
 if (import.meta.main) {
-  const text = await Deno.readTextFile("example");
+  const text = await Deno.readTextFile("input");
   const lines = text.split("\n");
   const graph = new Graph(lines);
+
+  console.log(`Calculating for ${graph.targetVertexIds.length} target(s)`);
+  for (const targetId of graph.targetVertexIds) {
+    const cost = dijkstra(graph, graph.startVertexId, targetId);
+    console.log("* Cost: " + cost);
+  }
 
   console.log(graph.vertices.length);
 }
