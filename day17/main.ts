3d79ff93b0ecf16e86e5b1e465d4d793b7afca09
Paul Rijneveld <paul.rijneveld@mendix.com>
2024-12-24 20:16:54 +0100

    Init day 17

---
+++ day17/main.ts
@@ -0,0 +1,36 @@
+class Registers {
+  a: number;
+  b: number;
+  c: number;
+  
+  constructor(registerLines: string[]) {
+    this.a = this.#readRegister(registerLines[0]);
+    this.b = this.#readRegister(registerLines[1]);
+    this.c = this.#readRegister(registerLines[2]);
+  }
+
+  #readRegister(line: string) {
+    return parseInt(line.split(" ")[2]);
+  }
+}
+
+class Program {
+  instructions: number[];
+
+  constructor(programLine: string) {
+    this.instructions = programLine.split(" ")[1].split(",").map(t => parseInt(t));
+  }
+}
+
+if (import.meta.main) {
+  const text = await Deno.readTextFile("example");
+  const parts = text.split("\n\n");
+  const registerLines = parts[0].split("\n");
+  const programLine = parts[1];
+
+  const registers = new Registers(registerLines);
+  const program = new Program(programLine);
+
+  console.log(registers);
+  console.log(program);
+}
