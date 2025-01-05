ccf5f729ff4167da425f677bcfbc082fcdfcfced
Paul Rijneveld <paul.rijneveld@mendix.com>
2025-01-05 21:59:05 +0100

    Split files and add permutations

---
+++ day21/directions.ts
@@ -0,0 +1,126 @@
+import { NumPad } from "./numpad.ts";
+
+export type Controls = "^" | "v" | "<" | ">" | "A";
+
+export class Directions {
+  from: string;
+  to: string;
+  numLeft: number;
+  numRight: number;
+  numUp: number;
+  numDown: number;
+
+  constructor(
+    from: string,
+    to: string,
+    numLeft: number,
+    numRight: number,
+    numUp: number,
+    numDown: number,
+  ) {
+    this.from = from;
+    this.to = to;
+    this.numLeft = numLeft;
+    this.numRight = numRight;
+    this.numUp = numUp;
+    this.numDown = numDown;
+  }
+
+  // Also returns permutations passing the empty gap on the num pad, needs to be filtered out after
+  #getControlsPermutations(): Controls[][] {
+    const controlsLists = this.#getControlsTuple();
+    const nonZeroLists = controlsLists.filter((l) => l.length > 0);
+    const nonZeroCount = nonZeroLists.length;
+
+    if (nonZeroCount === 0 || nonZeroCount === 1) {
+      return controlsLists.concat();
+    }
+
+    if (nonZeroCount === 2) {
+      const [firstList, secondList] = nonZeroLists;
+
+      const [as, bs] = firstList.length >= secondList.length
+        ? [firstList, secondList]
+        : [secondList, firstList];
+
+      const a = as[0];
+      const b = bs[0];
+
+      if (as.length === 1) { // bs.length is also 1
+        return [[a, b], [b, a]];
+      }
+
+      if (as.length === 2 && bs.length === 1) {
+        return [
+          [a, a, b],
+          [a, b, a],
+          [b, a, a],
+        ];
+      }
+
+      if (as.length === 2 && bs.length === 2) {
+        return [
+          [a, a, b, b],
+          [a, b, a, b],
+          [b, a, a, b],
+          [b, a, b, a],
+          [b, b, a, a],
+        ];
+      }
+
+      if (as.length === 3 && bs.length === 1) {
+        return [
+          [a, a, a, b],
+          [a, a, b, a],
+          [a, b, a, a],
+          [b, a, a, a],
+        ];
+      }
+
+      if (as.length === 3 && bs.length === 2) {
+        return [
+          [a, a, a, b, b],
+          [a, a, b, a, b],
+          [a, b, a, a, b],
+          [b, a, a, a, b],
+          [a, a, b, b, a],
+          [a, b, a, b, a],
+          [b, a, a, b, a],
+          [a, b, b, a, a],
+          [b, a, b, a, a],
+          [b, b, a, a, a],
+        ];
+      }
+    }
+
+    throw new Error(`Cannot handle ${nonZeroCount} non-zero directions`);
+  }
+
+  #getControlsTuple(): [Controls[], Controls[], Controls[], Controls[]] {
+    return [
+      Array(this.numLeft).fill("<"),
+      Array(this.numRight).fill(">"),
+      Array(this.numUp).fill("^"),
+      Array(this.numDown).fill("v"),
+    ];
+  }
+
+  static fromStartEnd(from: string, to: string, numPad: NumPad): Directions {
+    const fromPos = numPad.posMap.get(from);
+    const toPos = numPad.posMap.get(to);
+
+    if (fromPos === undefined || toPos === undefined) {
+      throw new Error(`fromStartEnd received invalid from/to`);
+    }
+
+    const start = fromPos;
+    const end = toPos;
+
+    const numLeft = Math.max(start.x - end.x, 0);
+    const numRight = Math.max(end.x - start.x, 0);
+    const numUp = Math.max(start.y - end.y, 0);
+    const numDown = Math.max(end.y - start.y, 0);
+
+    return new Directions(from, to, numLeft, numRight, numUp, numDown);
+  }
+}
---
+++ day21/numpad.ts
@@ -0,0 +1,71 @@
+import { Controls, Directions } from "./directions.ts";
+import { Pos } from "./pos.ts";
+
+export class NumPad {
+  lines: string[];
+  posMap: Map<string, Pos>;
+  directionsMap: Map<string, Directions>;
+
+  constructor(lines: string[]) {
+    this.lines = lines;
+    this.posMap = new Map();
+
+    for (let y = 0; y < lines.length; y++) {
+      for (let x = 0; x < lines[y].length; x++) {
+        const c = lines[y].charAt(x);
+        this.posMap.set(c, new Pos(x, y));
+      }
+    }
+
+    this.directionsMap = this.buildDirectionMap(this.posMap);
+  }
+
+  simulatePath(startKey: string, controls: Controls[]): string[] {
+    const startPos = this.posMap.get(startKey);
+    if (startPos === undefined) {
+      throw new Error(`position for ${startKey} is undefined`);
+    }
+
+    let currPos = startPos;
+
+    const simulatedPath: string[] = [];
+    simulatedPath.push(startKey);
+
+    for (const control of controls) {
+      if (control === "<") {
+        currPos = currPos.left;
+      } else if (control === ">") {
+        currPos = currPos.right;
+      } else if (control === "^") {
+        currPos = currPos.up;
+      } else {
+        currPos = currPos.down;
+      }
+
+      simulatedPath.push(this.getNumKey(currPos));
+    }
+
+    return simulatedPath;
+  }
+
+  getNumKey(pos: Pos): string {
+    return this.lines[pos.y][pos.x];
+  }
+
+  buildDirectionMap(posMap: Map<string, Pos>): Map<string, Directions> {
+    const directionsMap: Map<string, Directions> = new Map();
+    const keyPadKeys = [...posMap.keys().filter((k) => k !== "X")];
+
+    for (const from of keyPadKeys) {
+      for (const to of [...keyPadKeys]) {
+        const combinedKey = from + to;
+        directionsMap.set(
+          combinedKey,
+          Directions.fromStartEnd(from, to, this),
+        );
+      }
+    }
+
+    return directionsMap;
+  }
+}
