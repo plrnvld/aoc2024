0515818388859de440d0524749034dad093616d5
Paul Rijneveld <paul.rijneveld@mendix.com>
2025-01-10 22:28:48 +0100

    Solve day 21 part 1

--- day21/controlOptionsList.ts
+++ day21/controlOptionsList.ts
@@ -8,7 +8,30 @@ export class ControlOptionsList {
   }
 
   push(controlOptions: ArrowKey[][]) {
-    const arrowKeysAsStrings = controlOptions.map(arrows => arrows.join("") + "A"); // Active all control options
+    const arrowKeysAsStrings = controlOptions.map((arrows) =>
+      arrows.join("") + "A"
+    ); // Active all control options
     this.list.push(arrowKeysAsStrings);
+  }
+
+  toStrings(): string[] {
+    let aggr: string[] = this.list[0];
+
+    for (let pos = 1; pos < this.list.length; pos++) {
+      const newAggr: string[] = [];
+      for (const ag of aggr) {
+        if (this.list[pos].length === 0) {
+          newAggr.push(ag + "A"); // Active when youre already there
+        } else {
+          for (const option of this.list[pos]) {
+            newAggr.push(ag.concat(option));
+          }
+        }
+      }
+
+      aggr = newAggr;
+    }
+
+    return aggr;
   }
 }
--- day21/main.ts
+++ day21/main.ts
@@ -1,16 +1,17 @@
 import { ControlOptionsList } from "./controlOptionsList.ts";
 import { Pad } from "./pad.ts";
 
-function sequenceToTypeNumPad(nums: string, numPad: Pad): ControlOptionsList {
+function sequenceToTypePad(keys: string, pad: Pad): ControlOptionsList {
   const directionsList = new ControlOptionsList();
-  
-  const requiredKeys = "A" + nums;
+
+  const requiredKeys = "A" + keys;
   for (let i = 0; i < requiredKeys.length - 1; i++) {
     const pair = requiredKeys.slice(i, i + 2);
-    
-    const directionsOptions = numPad.directionsMap.get(pair);
-    if (directionsOptions === undefined || directionsOptions.validControlOptions.length === 0)
+
+    const directionsOptions = pad.directionsMap.get(pair);
+    if (directionsOptions === undefined) {
       throw new Error(`No valid options found for '${pair}'`);
+    }
 
     directionsList.push(directionsOptions.validControlOptions);
   }
@@ -18,19 +19,75 @@ function sequenceToTypeNumPad(nums: string, numPad: Pad): ControlOptionsList {
   return directionsList;
 }
 
+function lengthShortest(texts: string[]): number {
+  let res = texts[0].length;
+
+  for (let i = 1; i < texts.length; i++) {
+    res = Math.min(res, texts[i].length);
+  }
+
+  return res;
+}
+
+function calcBestSequence(input: string, numPad: Pad, arrowPad: Pad): number {
+  const numPadOptions = sequenceToTypePad(input, numPad);
+
+  const numPadOptionsStrings = numPadOptions.toStrings();
+
+  const allNumPad1Solutions = [];
+
+  for (const numPadOptionString of numPadOptionsStrings) {
+    const options = sequenceToTypePad(
+      numPadOptionString,
+      arrowPad,
+    );
+
+    for (const result of options.toStrings()) {
+      allNumPad1Solutions.push(result);
+    }
+  }
+
+  const allNumPad2Solutions = [];
+
+  for (const numPadOptionString of allNumPad1Solutions) {
+    const options = sequenceToTypePad(
+      numPadOptionString,
+      arrowPad,
+    );
+
+    for (const result of options.toStrings()) {
+      allNumPad2Solutions.push(result);
+    }
+  }
+
+  console.log();
+  console.log("*****");
+    
+  const shortest = lengthShortest(allNumPad2Solutions);
+  const inputNum = parseInt(input.split("").filter(c => c >= '0' && c <= '9').join(""));
+  const outcome = inputNum * shortest;
+  console.log(`${shortest} x ${inputNum} = ${outcome}`);
+
+  return outcome;
+}
+
 if (import.meta.main) {
   const numPadText = await Deno.readTextFile("numpad");
   const numPadLines = numPadText.split("\n");
-  const pad = new Pad(numPadLines);
 
-  // const text = await Deno.readTextFile("example");
-  // const lines = text.split("\n");
+  const arrowPadText = await Deno.readTextFile("arrowpad");
+  const arrowPadLines = arrowPadText.split("\n");
 
-  // console.log(pad.directionsMap);
-  // console.log(pad.directionsMap.keys().toArray().length);
+  const inputText = await Deno.readTextFile("input");
+  const inputLines = inputText.split("\n");
 
-  const controlOptions = sequenceToTypeNumPad("029A", pad);
+  const numPad = new Pad(numPadLines);
+  const arrowPad = new Pad(arrowPadLines);
 
-  console.log(JSON.stringify(controlOptions));
+  let sum = 0;
+
+  for (const input of inputLines)
+    sum += calcBestSequence(input, numPad, arrowPad);
+
+  console.log(sum);
 }
-
