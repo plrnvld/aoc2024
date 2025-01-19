44ed22fd19288e2c7841a3b039f670239dc947dd
Paul Rijneveld <paul.rijneveld@mendix.com>
2025-01-19 17:49:20 +0100

    Solve day 22 part 2

--- day22/main.ts
+++ day22/main.ts
@@ -1,24 +1,33 @@
 export function modulo(num: bigint, wrap: bigint): bigint {
-  if (num < 0) {
-    return (wrap + (num % wrap)) % wrap;
-  }
-
   return num % wrap;
 }
 
-function nextSecret(secret: bigint): bigint {
+function nextSecret(secret: number): number {
   let newSecret = secret;
-  newSecret = mixPrune(newSecret, (n) => n * 64n);
-  newSecret = mixPrune(newSecret, (n) => n / 32n);
-  newSecret = mixPrune(newSecret, (n) => n * 2048n);
+  newSecret = newSecret ^ (newSecret * 64) % 16777216;
+  newSecret = newSecret ^ (newSecret / 32) % 16777216;
+  let newSecretBig = BigInt(newSecret);
+  newSecretBig = newSecretBig ^ (newSecretBig * 2048n) % 16777216n;
 
-  return newSecret;
+  return Number(newSecretBig);
 }
 
 function mixPrune(secret: bigint, mixValueFunc: (n: bigint) => bigint) {
   const mixValue = mixValueFunc(secret);
-  const newSecret = mix(secret, mixValue);
-  return prune(newSecret);
+  const newSecret = secret ^ mixValue;
+  return newSecret % 16777216n;
+}
+
+function mixPrune64(secret: bigint) {
+  return secret ^ (secret * 64n) % 16777216n;
+}
+
+function mixPrune32(secret: bigint) {
+  return secret ^ (secret / 32n) % 16777216n;
+}
+
+function mixPrune2048(secret: bigint) {
+  return secret ^ (secret * 2048n) % 16777216n;
 }
 
 function mix(secret: bigint, value: bigint): bigint {
@@ -29,63 +38,19 @@ function prune(secret: bigint): bigint {
   return modulo(secret, 16777216n);
 }
 
-function indexOfChanges(
-  prices: number[],
-  changes: [number, number, number, number],
-): number | undefined {
-  const currChanges: number[] = [];
-
-  for (let i = 1; i < prices.length; i++) {
-    const change = prices[i] - prices[i - 1];
-    const currChangesLen = currChanges.push(change);
-    if (currChangesLen === 5) {
-      currChanges.shift();
-    }
-
-    if (currChanges.length === 4) {
-      let isMatch = true;
-
-      for (let p = 0; p < 4; p++) {
-        if (changes[p] !== currChanges[p]) {
-          isMatch = false;
-        }
-      }
-
-      if (isMatch) {
-        return i;
-      }
-    }
-  }
-
-  return undefined;
-}
-
-function getPrices(secret: bigint): number[] {
-  const prices: number[] = [];
-
-  prices.push(Number(secret % 10n));
-
-  let temp = secret;
-  for (let i = 0; i < 2000; i++) {
-    temp = nextSecret(temp);
-    prices.push(Number(temp % 10n));
-  }
-
-  return prices;
-}
-
 function getMatchingPriceWhileBuildingPrices(
-  secret: bigint,
+  secret: number,
   changes: [number, number, number, number],
 ): number | undefined {
   let currSecret = secret;
-  let prevPrice = Number(secret % 10n);
+  let prevPrice = secret % 10;
 
   let matchPos = 0;
 
   for (let i = 0; i < 2000; i++) {
     currSecret = nextSecret(currSecret);
-    const currPrice = Number(currSecret % 10n);
+
+    const currPrice = currSecret % 10;
     const currChange = currPrice - prevPrice;
 
     if (currChange === changes[matchPos]) {
@@ -106,39 +71,94 @@ function getMatchingPriceWhileBuildingPrices(
   return undefined;
 }
 
+function inBounds(
+  diff1: number,
+  diff2: number,
+  diff3: number,
+  diff4: number,
+): boolean {
+  let lowest = Number.MAX_VALUE;
+  let highest = Number.MIN_VALUE;
+
+  let temp = 0;
+
+  temp = temp + diff1;
+  lowest = Math.min(temp, lowest);
+  highest = Math.max(temp, highest);
+
+  temp = temp + diff2;
+  lowest = Math.min(temp, lowest);
+  highest = Math.max(temp, highest);
+
+  temp = temp + diff3;
+  lowest = Math.min(temp, lowest);
+  highest = Math.max(temp, highest);
+
+  temp = temp + diff4;
+  lowest = Math.min(temp, lowest);
+  highest = Math.max(temp, highest);
+
+  return highest - lowest < 10;
+}
+
+function isImprovement(diff1: number, diff4: number) {
+    return diff4 > -diff1;
+}
+
+// When the absolute diff changes are low, there is more chance all prices are high
+function isLowChange(diff1: number, diff2: number, diff3: number, diff4: number) {
+  const limit = 4;
+  const low1 = Math.abs(diff1) < limit;
+  const low2 = Math.abs(diff1) < limit;
+  const low3 = Math.abs(diff1) < limit;
+  const low4 = Math.abs(diff1) < limit;
+
+  const sumLow = diff1 + diff2 + diff3 + diff4 < limit;
+
+  return low1 && low2 && low3 && low4 && sumLow;
+}
+
 if (import.meta.main) {
-  const text = await Deno.readTextFile("part2example");
+  const text = await Deno.readTextFile("input");
   const lines = text.split("\n");
-  const secrets = lines.map((line) => BigInt(line));
+  const secrets = lines.map((line) => parseInt(line));
 
   const scoreMap: Map<string, number> = new Map();
 
-  let secretIndex = 0;
+  let diffIndex = 0;
 
   const start = performance.now();
 
-  for (const secret of secrets) {
-    console.log(`Processing ${secretIndex++}`);
+  const totalDiffs = Math.pow(19, 4);
 
-    for (let diff1 = -9; diff1 < 10; diff1++) {
-      for (let diff2 = -9; diff2 < 10; diff2++) {
-        for (let diff3 = -9; diff3 < 10; diff3++) {
-          for (let diff4 = -9; diff4 < 10; diff4++) {
-            const key = diff1.toString() + diff2.toString() + diff3.toString() +
-              diff4.toString();
+  for (let diff1 = -9; diff1 < 10; diff1++) {
+    for (let diff2 = -9; diff2 < 10; diff2++) {
+      for (let diff3 = -9; diff3 < 10; diff3++) {
+        for (let diff4 = -9; diff4 < 10; diff4++) {
+          diffIndex++;
+          if (diffIndex % 1000 === 0) {
+            console.log(`Processing diffs ${diffIndex} (${Math.round(diffIndex / totalDiffs * 1000) / 10}%)`);
+          }
 
-            const priceFound = getMatchingPriceWhileBuildingPrices(secret, [
-              diff1,
-              diff2,
-              diff3,
-              diff4,
-            ]);
-            if (priceFound) {
-              const score = scoreMap.get(key);
-              if (score) {
-                scoreMap.set(key, score + priceFound);
-              } else {
-                scoreMap.set(key, priceFound);
+          if (inBounds(diff1, diff2, diff3, diff4) && isImprovement(diff1, diff4) && isLowChange(diff1, diff2, diff3, diff4) ) {
+            for (const secret of secrets) {
+              const key = diff1.toString() + diff2.toString() +
+                diff3.toString() +
+                diff4.toString();
+
+              const priceFound = getMatchingPriceWhileBuildingPrices(secret, [
+                diff1,
+                diff2,
+                diff3,
+                diff4,
+              ]);
+              if (priceFound) {
+                const score = scoreMap.get(key);
+                if (score) {
+                  scoreMap.set(key, score + priceFound);
+                } else {
+                  scoreMap.set(key, priceFound);
+                }
               }
             }
           }
@@ -161,5 +181,7 @@ if (import.meta.main) {
     }
   }
 
-  console.log("BEST = " + best + " with best key: " + bestKey);
+  console.log("Best result = " + best + " with changes: " + bestKey);
 }
+
+// BEST = 1710 with best key: 0,1,-1,1
