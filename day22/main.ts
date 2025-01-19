1cf7bd918e5e5ba37a0d0a47d22452c0d651dae2
Paul Rijneveld <paul.rijneveld@mendix.com>
2025-01-19 10:57:59 +0100

    Start calculating prices

--- day22/main.ts
+++ day22/main.ts
@@ -8,9 +8,9 @@ export function modulo(num: bigint, wrap: bigint): bigint {
 
 function nextSecret(secret: bigint): bigint {
   let newSecret = secret;
-  newSecret = mixPrune(newSecret, n => n * 64n);
-  newSecret = mixPrune(newSecret, n => n / 32n);
-  newSecret = mixPrune(newSecret, n => n * 2048n);
+  newSecret = mixPrune(newSecret, (n) => n * 64n);
+  newSecret = mixPrune(newSecret, (n) => n / 32n);
+  newSecret = mixPrune(newSecret, (n) => n * 2048n);
 
   return newSecret;
 }
@@ -29,22 +29,37 @@ function prune(secret: bigint): bigint {
   return modulo(secret, 16777216n);
 }
 
-let sum = 0n;
+function getPrices(secret: bigint): number[] {
+  const prices: number[] = [];
+
+
+  prices.push(Number(secret % 10n));
+
+  let temp = secret;
+  for (let i = 0; i < 9; i++) {
+    temp = nextSecret(temp);
+    prices.push(Number(temp % 10n));
+  }
+
+  return prices;
+}
 
 if (import.meta.main) {
   const text = await Deno.readTextFile("input");
   const lines = text.split("\n");
-  const secrets = lines.map(line => BigInt(line))
+  const secrets = lines.map((line) => BigInt(line));
+
+  let sum = 0n;
 
   for (let secret of secrets) {
     for (let i = 0; i < 2000; i++) {
       secret = nextSecret(secret);
-      
     }
 
-    // console.log(secret);
     sum += secret;
   }
 
   console.log(sum);
+
+  console.log(getPrices(123n));
 }
