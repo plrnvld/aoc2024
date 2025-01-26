9c12746c173c1d5de3d35e25517db926b4d9a33d
Paul Rijneveld <paul.rijneveld@mendix.com>
2025-01-26 22:46:41 +0100

    Solve day24 part 2

--- day24/main.ts
+++ day24/main.ts
@@ -3,34 +3,76 @@ type Op =
   | { op: "or"; left: string; right: string; output: string }
   | { op: "xor"; left: string; right: string; output: string };
 
-function calc(op: Op, leftValue: number, rightValue: number): number {
-  if (op.op === "and") {
-    return leftValue === 1 && rightValue === 1 ? 1 : 0;
-  }
-  if (op.op === "or") {
-    return leftValue === 0 && rightValue === 0 ? 0 : 1;
-  }
-  if (op.op === "xor") {
-    return leftValue !== rightValue ? 1 : 0;
+class OutputGroup {
+  outputOp: Op;
+  outputXorChild: Op;
+  outputOtherChild: Op;
+  terminalGrandChild?: Op;
+  nonTerminalGrandChild?: Op;
+
+  constructor(zNum: number, opsList: Op[]) {
+    const label = zNum < 10 ? `z0${zNum}` : `z${zNum}`;
+    const outputOp = opsList.find((op) => op.output === label);
+    if (outputOp === undefined) {
+      throw new Error("Cannot find output for num " + zNum);
+    }
+
+    this.outputOp = outputOp;
+    const leftOutputOp = opsList.find((op) => op.output === outputOp.left);
+    if (leftOutputOp === undefined) {
+      throw new Error("Cannot find leftOutputOp for num " + zNum);
+    }
+    const rightOutputop = opsList.find((op) => op.output === outputOp.right);
+    if (rightOutputop === undefined) {
+      throw new Error("Cannot find leftOutputOp for num " + zNum);
+    }
+
+    const [xorChild, otherChild] = leftOutputOp.op === "xor"
+      ? [leftOutputOp, rightOutputop]
+      : [rightOutputop, leftOutputOp];
+
+    if (xorChild.op !== "xor" && zNum !== 45) {
+      throw new Error("Cannot find xorChild for num " + zNum);
+    }
+
+    this.outputXorChild = xorChild;
+    this.outputOtherChild = otherChild;
+
+    if (zNum > 1) {
+      const grandChild1 = opsList.find((op) => op.output === otherChild.left);
+      if (grandChild1 === undefined)
+        throw new Error("Cannot find grandChild1 for num " + zNum);
+      if (grandChild1.op !== "and")
+        throw new Error("No AND for grandChild1 for num " + zNum);
+
+      const grandChild2 = opsList.find((op) => op.output === otherChild.right);
+      if (grandChild2 === undefined)
+        throw new Error("Cannot find grandChild2 for num " + zNum);
+      if (grandChild2.op !== "and")
+        throw new Error("No AND for grandChild2 for num " + zNum);
+    }
   }
 
-  throw new Error(`Cannot calculate operator`);
+  print() {
+    this.printOp(this.outputOp);
+    this.printOp(this.outputXorChild);
+    this.printOp(this.outputOtherChild);
+    console.log();
+  }
+
+  printOp(op: Op) {
+    const [firstOp, secondOp] = op.left.startsWith("y") ? [op.right, op.left] : [op.left, op.right];
+
+    console.log(`${firstOp} ${op.op.toUpperCase()} ${secondOp} -> ${op.output}`);
+  }
 }
 
 if (import.meta.main) {
-  const text = await Deno.readTextFile("input");
-  const parts = text.split("\n\n");
+  const text = await Deno.readTextFile("clean");
+  const lines = text.split("\n");
 
-  const inputMap: Map<string, number> = new Map();
-  for (const line of parts[0].split("\n")) {
-    const wire = line.split(": ");
-    const key = wire[0];
-    const value = parseInt(wire[1]);
-    inputMap.set(key, value);
-  }
-
-  let opsList: Op[] = [];
-  for (const line of parts[1].split("\n")) {
+  const opsList: Op[] = [];
+  for (const line of lines) {
     const expr = line.split(" -> ");
     const output = expr[1];
     const input = expr[0].split(" ");
@@ -46,53 +88,14 @@ if (import.meta.main) {
     );
   }
 
-  let skipped: Op[] = [];
-
-  console.log(`Starting with ${opsList.length} operations`);
-  // for (const oper of opsList) {
-  //   console.log(`OPER: ${JSON.stringify(oper)}`);
-  // }
-
-  let round = 1;
-  while (opsList.filter((op) => op.output.startsWith("z")).length > 0) {
-    const curr = opsList.pop()!;
-
-    const leftVal = inputMap.get(curr.left);
-    const rightVal = inputMap.get(curr.right);
-    const outputVal = inputMap.get(curr.output);
-
-    if (outputVal !== undefined) {
-      throw new Error(`Output ${curr.output} already exists`);
-    }
-
-    if (leftVal !== undefined && rightVal !== undefined) {
-      const result = calc(curr, leftVal, rightVal);
-      inputMap.set(curr.output, result);
-      console.log(`> Adding ${curr.output} to input`);
-    } else {
-      skipped.push(curr);
-    }
-
-    opsList = skipped.concat(opsList);
-    skipped = [];
+  const groups: OutputGroup[] = [];
+  for (let i = 1; i <= 45; i++) {
+    const group = new OutputGroup(i, opsList);
+    groups.push(group);
+    group.print();
   }
 
-  console.log("*********\n");
-
-  const zList: { zNum: number; res: number }[] = [];
-  for (const [input, value] of inputMap) {
-    if (input.startsWith("z")) {
-      const zNum = parseInt(input.slice(1));
-      
-      zList.push({ zNum: zNum, res: value });
-    }
-  }
-
-  const zSorted = zList.toSorted((z1, z2) => z2.zNum - z1.zNum);
-  const zBits = zSorted.map((n) => n.res.toString()).join("");
-
-  console.log(zSorted);
-  console.log(zBits);
-
-  console.log(parseInt(zBits, 2));
+  console.log(groups.length);
 }
+
+// hjf, kdh, kpp, sgj, vss, z14, z31, z35 (from notes and from updating file 'clean')
